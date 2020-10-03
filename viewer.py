2dcb92c287e09913025babcfe022cfd817516918
George Berdovskiy <georgeberdovskiy@Georges-iMac.local>
2020-10-03 14:03:57 -0700

    Retrieve all XML from 2015

--- viewer.py
+++ viewer.py
@@ -3,17 +3,43 @@ from flask import Flask, render_template
 import xml.etree.ElementTree as ET
 import urllib.request
 import xmltodict
+from bs4 import BeautifulSoup
+import ftplib
+
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def view():
-	url = 'ftp://data.asc-csa.gc.ca/users/OpenData_DonneesOuvertes/pub/NEOSSAT/XML/NEOS_SCI_2015347000000-2018-10-15-12-29-28.xml'
-	response = urllib.request.urlopen(url).read()
-	'''tree = ET.fromstring(response)'''
-	data = xmltodict.parse(response)
+	all_xml_dict = []
 
-	return render_template("viewer.html", message="message", test_obj=data)
+	with ftplib.FTP('data.asc-csa.gc.ca') as ftp:
+		try:
+			ftp.login()
+			ftp.cwd('/users/OpenData_DonneesOuvertes/pub/NEOSSAT/XML/')
+			files = []
+			'''ftp.dir(files.append)
+			print(files)'''
+			filenames = ftp.nlst()
+
+			iterations = 0
+
+			for filename in filenames:
+				if "2015" in filename and iterations < 5 :
+					iterations += 1
+					url = 'ftp://data.asc-csa.gc.ca/users/OpenData_DonneesOuvertes/pub/NEOSSAT/XML/' + filename
+					response = urllib.request.urlopen(url).read()
+					data = xmltodict.parse(response)
+					all_xml_dict.append(data)
+				if iterations is 5:
+					print(all_xml_dict)
+					break
+
+		except ftplib.all_errors as e:
+			print('FTP error:', e)
+
+	return render_template("viewer.html", message="message", test_obj=all_xml_dict)
 
 if __name__ == '__main__':
 	app.run(debug=True)
