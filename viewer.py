0c7e153be3a255c1cbc04c88af44b72c9afc3844
George Berdovskiy <georgeberdovskiy@Georges-iMac.local>
2020-10-03 13:25:12 -0700

    Retrieve One CSA XML File

--- viewer.py
+++ viewer.py
@@ -1,9 +1,19 @@
 from flask import Flask, render_template
+
+import xml.etree.ElementTree as ET
+import urllib.request
+import xmltodict
+
 app = Flask(__name__)
 
 @app.route('/')
 def view():
-    return render_template("viewer.html", message="message")
+	url = 'ftp://data.asc-csa.gc.ca/users/OpenData_DonneesOuvertes/pub/NEOSSAT/XML/NEOS_SCI_2015347000000-2018-10-15-12-29-28.xml'
+	response = urllib.request.urlopen(url).read()
+	'''tree = ET.fromstring(response)'''
+	data = xmltodict.parse(response)
+
+	return render_template("viewer.html", message="message", test_obj=data)
 
 if __name__ == '__main__':
 	app.run(debug=True)
