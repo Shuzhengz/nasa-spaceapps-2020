32e30a2db17e04ca57e57092b6d883c65c0a511c
shuzhengz <treez.zhang@gmail.com>
2020-10-04 16:23:34 -0700

    add search function

--- viewer.py
+++ viewer.py
@@ -1,7 +1,7 @@
 import io
 import base64
 
-from flask import Flask, Response, render_template, request
+from flask import Flask, Response, render_template, request, redirect, url_for
 from functions import images, spectral
 
 import urllib.request
@@ -16,7 +16,6 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 
-
 from astropy.utils.data import download_file, get_pkg_data_filename
 from astropy.io import fits
 
@@ -26,78 +25,96 @@ plt.style.use(astropy_mpl_style)
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def view():
-	all_xml_dict = []
+    all_xml_dict = []
 
-	manual_urls = ["ftp://data.asc-csa.gc.ca/users/OpenData_DonneesOuvertes/pub/NEOSSAT/XML/NEOS_SCI_2018284225800-2018-10-15-02-00-25.xml", "ftp://data.asc-csa.gc.ca/users/OpenData_DonneesOuvertes/pub/NEOSSAT/XML/NEOS_SCI_2018251042635-2018-10-15-01-59-42.xml", "ftp://data.asc-csa.gc.ca/users/OpenData_DonneesOuvertes/pub/NEOSSAT/XML/NEOS_SCI_2018232040619-2018-10-15-10-49-24.xml", "ftp://data.asc-csa.gc.ca/users/OpenData_DonneesOuvertes/pub/NEOSSAT/XML/NEOS_SCI_2018033164500-2018-10-15-12-54-57.xml", "ftp://data.asc-csa.gc.ca/users/OpenData_DonneesOuvertes/pub/NEOSSAT/XML/NEOS_SCI_2017283104140-2018-10-15-12-49-38.xml"]
+    manual_urls = [
+        "ftp://data.asc-csa.gc.ca/users/OpenData_DonneesOuvertes/pub/NEOSSAT/XML/NEOS_SCI_2018284225800-2018-10-15-02-00-25.xml",
+        "ftp://data.asc-csa.gc.ca/users/OpenData_DonneesOuvertes/pub/NEOSSAT/XML/NEOS_SCI_2018251042635-2018-10-15-01-59-42.xml",
+        "ftp://data.asc-csa.gc.ca/users/OpenData_DonneesOuvertes/pub/NEOSSAT/XML/NEOS_SCI_2018232040619-2018-10-15-10-49-24.xml",
+        "ftp://data.asc-csa.gc.ca/users/OpenData_DonneesOuvertes/pub/NEOSSAT/XML/NEOS_SCI_2018033164500-2018-10-15-12-54-57.xml",
+        "ftp://data.asc-csa.gc.ca/users/OpenData_DonneesOuvertes/pub/NEOSSAT/XML/NEOS_SCI_2017283104140-2018-10-15-12-49-38.xml"]
 
-	for url in manual_urls:
-		response = urllib.request.urlopen(url).read()
-		data = xmltodict.parse(response, dict_constructor=dict)
-		all_xml_dict.append(data)
+    for url in manual_urls:
+        response = urllib.request.urlopen(url).read()
+        data = xmltodict.parse(response, dict_constructor=dict)
+        all_xml_dict.append(data)
 
-	'''with ftplib.FTP('data.asc-csa.gc.ca') as ftp:
-		try:
-			ftp.login()
-			ftp.cwd('/users/OpenData_DonneesOuvertes/pub/NEOSSAT/XML/')
-			files = []
+    '''with ftplib.FTP('data.asc-csa.gc.ca') as ftp:
+        try:
+            ftp.login()
+            ftp.cwd('/users/OpenData_DonneesOuvertes/pub/NEOSSAT/XML/')
+            files = []
 
-			filenames = ftp.nlst()
+            filenames = ftp.nlst()
 
-			iterations = 0
+            iterations = 0
 
-			for filename in filenames[::-1]:
-				if iterations < 1:
-					iterations += 1
-					url = 'ftp://data.asc-csa.gc.ca/users/OpenData_DonneesOuvertes/pub/NEOSSAT/XML/' + filename
-					response = urllib.request.urlopen(url).read()
-					data = xmltodict.parse(response, dict_constructor=dict)
-					all_xml_dict.append(data)
-				if iterations == 1:
-					break
+            for filename in filenames[::-1]:
+                if iterations < 1:
+                    iterations += 1
+                    url = 'ftp://data.asc-csa.gc.ca/users/OpenData_DonneesOuvertes/pub/NEOSSAT/XML/' + filename
+                    response = urllib.request.urlopen(url).read()
+                    data = xmltodict.parse(response, dict_constructor=dict)
+                    all_xml_dict.append(data)
+                if iterations == 1:
+                    break
 
-		except ftplib.all_errors as e:
-			print('FTP error:', e)'''
+        except ftplib.all_errors as e:
+            print('FTP error:', e)'''
 
-	'''get_image("")'''
-	'''print(all_xml_dict[0]['FITSImage']['Image']['ImageFile'])'''
+    '''get_image("")'''
+    '''print(all_xml_dict[0]['FITSImage']['Image']['ImageFile'])'''
 
-	'''# Generate plot
-	fig = Figure()
-	axis = fig.add_subplot(1, 1, 1)
-	axis.set_title("title")
-	axis.set_xlabel("x-axis")
-	axis.set_ylabel("y-axis")
-	axis.grid()
-	axis.plot(range(5), range(5), "ro-")
+    '''# Generate plot
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+    axis.set_title("title")
+    axis.set_xlabel("x-axis")
+    axis.set_ylabel("y-axis")
+    axis.grid()
+    axis.plot(range(5), range(5), "ro-")
 
-	# Convert plot to PNG image
-	pngImage = io.BytesIO()
-	FigureCanvas(fig).print_png(pngImage)
+    # Convert plot to PNG image
+    pngImage = io.BytesIO()
+    FigureCanvas(fig).print_png(pngImage)
 
-	# Encode PNG image to base64 string
-	pngImageB64String = "data:image/png;base64,"
-	pngImageB64String += base64.b64encode(pngImage.getvalue()).decode('utf8')'''
+    # Encode PNG image to base64 string
+    pngImageB64String = "data:image/png;base64,"
+    pngImageB64String += base64.b64encode(pngImage.getvalue()).decode('utf8')'''
 
-	'''image_file = download_file('ftp://ftp.asc-csa.gc.ca/users/OpenData_DonneesOuvertes/pub/NEOSSAT/ASTRO/2018/284/TOI129/FINE_POINT/NEOS_SCI_2018284225800.fits')
-	image_data = fits.getdata(image_file, ext=0)
-	image_plt = plt.imshow(image_data, cmap='gray')'''
+    '''image_file = download_file('ftp://ftp.asc-csa.gc.ca/users/OpenData_DonneesOuvertes/pub/NEOSSAT/ASTRO/2018/284/TOI129/FINE_POINT/NEOS_SCI_2018284225800.fits')
+    image_data = fits.getdata(image_file, ext=0)
+    image_plt = plt.imshow(image_data, cmap='gray')'''
 
-	'''get_image('ftp://ftp.asc-csa.gc.ca/users/OpenData_DonneesOuvertes/pub/NEOSSAT/NESS/2015/347-NESS/NEOS_SCI_2015347071100.fits')'''
+    '''get_image('ftp://ftp.asc-csa.gc.ca/users/OpenData_DonneesOuvertes/pub/NEOSSAT/NESS/2015/347-NESS/NEOS_SCI_2015347071100.fits')'''
 
-	return render_template("viewer.html", message="message", test_obj=all_xml_dict)
+    return render_template("viewer.html", message="message", test_obj=all_xml_dict)
+
 
 @app.route('/detail')
 def details():
-	datetime = request.args.get('datetime', default = "-1", type = str)
-	
-	return render_template("detail.html", datetime=datetime)
+    datetime = request.args.get('datetime', default="-1", type=str)
+
+    return render_template("detail.html", datetime=datetime)
+
 
 def get_image(url):
-	'''image_file = download_file('ftp://ftp.asc-csa.gc.ca/users/OpenData_DonneesOuvertes/pub/NEOSSAT/ASTRO/2018/284/TOI129/FINE_POINT/NEOS_SCI_2018284225800.fits')
-	image_data = fits.getdata(image_file, ext=0)
-	image_plt = plt.imshow(image_data, cmap='gray')'''
+    '''image_file = download_file('ftp://ftp.asc-csa.gc.ca/users/OpenData_DonneesOuvertes/pub/NEOSSAT/ASTRO/2018/284/TOI129/FINE_POINT/NEOS_SCI_2018284225800.fits')
+    image_data = fits.getdata(image_file, ext=0)
+    image_plt = plt.imshow(image_data, cmap='gray')'''
+
+
+@app.route("/search", methods=["POST", "GET"])
+def search():
+    if request.method == "POST":
+        user = request.form["type"]
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template("search.html")
+
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
